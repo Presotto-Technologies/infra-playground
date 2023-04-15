e6e3af7800f3233744d09d7f14171aedcbdc1955
Robinson Presotto <robipresotto@gmail.com>
2023-04-15 20:42:34 +0200

    Bump prometheus version

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -3,7 +3,7 @@ resource "helm_release" "prometheus" {
   name = "prometheus"
   namespace = "monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
-  version = "19.7.2"
+  version = "20.0.2"
 
   values = [
     templatefile("${path.module}/templates/prometheus-values.yaml", {
