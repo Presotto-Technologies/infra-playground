c3ebe398bd69f8c49d3fe4205f212bb45d7d5533
Robinson Presotto <robipresotto@gmail.com>
2023-04-22 19:40:26 +0200

    Setup elasticsearch

---
+++ terraform/elasticsearch.tf
@@ -0,0 +1,14 @@
+resource "helm_release" "elasticsearch" {
+  chart = "elasticsearch"
+  name = "elasticsearch"
+  repository = "https://helm.elastic.co"
+  namespace = "db"
+  version = "8.5.1"
+  
+  values = [
+    templatefile("${path.module}/templates/elastic-values.yaml", {
+      // not definied yet
+    })
+  ]
+
+}
