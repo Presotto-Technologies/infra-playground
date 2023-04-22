16a3d1bd91665d1f85d7b02ba7280a84d47d7ce8
Robinson Presotto <robipresotto@gmail.com>
2023-04-22 22:48:14 +0200

    Add secrets for elastic app

--- terraform/app.tf
+++ terraform/app.tf
@@ -14,6 +14,22 @@ resource "kubernetes_secret" "redis-cluster-password" {
 
 }
 
+resource "kubernetes_secret" "elastic-search-password" {
+  metadata {
+    name = "elastic-search-password"
+    namespace = "app"
+  }
+
+  data = {
+    password = random_password.elastic-search-password.result
+  }
+
+  depends_on = [
+    helm_release.elasticsearch
+  ]
+
+}
+
 # resource "kubernetes_secret" "openai-token" {
 #   metadata {
 #     name = "openai-token"
